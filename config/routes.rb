18d737a268c4b7f297b7a3089a170797e007d04a
faizan butt <buttfaizan431@gmail.com>
2021-02-13 13:38:12 +0500

    routes refactor

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
 
   get 'login', to: 'logins#new'
   post 'login', to: 'logins#create'
-  delete 'logout', to: 'logins#destroy'
+  get 'logout', to: 'logins#destroy'
 
-  post 'course_enroll', to: 'student_courses#create'
+  get 'course_enroll', to: 'student_courses#create'
 
 end
